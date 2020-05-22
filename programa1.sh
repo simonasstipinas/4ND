2b039902d47158fb06bfe0a22e344fe65f50fb12
simonass <simonas.stipinas@programmatus.com>
2020-05-22 12:09:40 +0300

    minor fixes

--- programa1.sh
+++ programa1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 SBATCH -p short # eilė
 SBATCH -N1 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
 SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
--- programa3.sh
+++ programa3.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 SBATCH -p short # eilė
 SBATCH -N4 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
 SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
