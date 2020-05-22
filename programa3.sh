5ad23921a449e0e68a3e8086a6e08b927fe3319c
simonass <simonas.stipinas@programmatus.com>
2020-05-22 12:13:26 +0300

    minor fixes

--- programa3.sh
+++ programa3.sh
@@ -1,6 +1,5 @@
-!/bin/sh
-SBATCH -p short # eilė
-SBATCH -N4 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
-SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
-#mpicc -g -Wall -o main main.c
+#!/bin/sh
+#SBATCH -p short # eilė
+#SBATCH -N4 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
+#SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
 mpiexec -n 4 ./main
