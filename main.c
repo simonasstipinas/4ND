0be62841bbc8b1a6050a858b0873f77af1be675c
simonass <simonas.stipinas@programmatus.com>
2020-05-22 11:51:01 +0300

    minor fixes

--- main.c
+++ main.c
@@ -2,100 +2,95 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
+#include <stdbool.h>
 
 void merge(int *, int *, int, int, int);
+
 void mergeSort(int *, int *, int, int);
 
-int main(int argc, char** argv) {
+int cmpfunc (const void * a, const void * b) {
+    return ( *(int*)a - *(int*)b );
+}
 
-    /********** Create and populate the array **********/
-    int n = 130000000; //atoi(argv[1]);
-    int *original_array = malloc(n * sizeof(int));
+int main(int argc, char **argv) {
+
+    int n = atoi(argv[1]);
+    int *original_array = NULL;
+    int *copy_original_array = NULL;
 
     int c;
-    srand(time(NULL));
-    printf("This is the unsorted array: ");
-    for(c = 0; c < n; c++) {
-
-        original_array[c] = c % 20;
-//        printf("%d ", original_array[c]);
-
-    }
-
-    printf("\n");
-    printf("\n");
-
-    /********** Initialize MPI **********/
     int world_rank;
     int world_size;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    if (world_rank == 0) {
+        original_array = malloc(n * sizeof(int));
+        copy_original_array = malloc(n * sizeof(int));
+        srand(1);
+        for (c = 0; c < n; c++) {
+            int temp = rand() % 5;
+            original_array[c] = temp;
+            copy_original_array[c] = temp;
+        }
+        qsort(copy_original_array, n, sizeof(int), cmpfunc);
+    }
+    int size = n / world_size;
 
-    /********** Divide the array in equal-sized chunks **********/
-    int size = n/world_size;
-
-    /********** Send each subarray to each process **********/
     int *sub_array = malloc(size * sizeof(int));
     MPI_Scatter(original_array, size, MPI_INT, sub_array, size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    /********** Perform the mergesort on each process **********/
     int *tmp_array = malloc(size * sizeof(int));
     clock_t t;
     t = clock();
+
     mergeSort(sub_array, tmp_array, 0, (size - 1));
 
-    /********** Gather the sorted subarrays into one **********/
     int *sorted = NULL;
-    if(world_rank == 0) {
-
+    if (world_rank == 0) {
         sorted = malloc(n * sizeof(int));
-
     }
-
     MPI_Gather(sub_array, size, MPI_INT, sorted, size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    /********** Make the final mergeSort call **********/
-    if(world_rank == 0) {
-
+    if (world_rank == 0) {
         int *other_array = malloc(n * sizeof(int));
+
+
+
+//        for (c = 0; c < n; c++) {
+//            printf("%d ", copy_original_array[c]);
+//        }
+//        printf("\n");
+
         mergeSort(sorted, other_array, 0, (n - 1));
 
         t = clock() - t;
-        double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        double time_taken = ((double) t) / CLOCKS_PER_SEC; // in seconds
+        printf("Programa užtruko: %f sekundes \n", time_taken);
+        printf("\n");
 
-        printf("fun() took %f seconds to execute \n", time_taken);
 
-        /********** Display the sorted array **********/
-//        printf("This is the sorted array: ");
-//        for(c = 0; c < n; c++) {
-//
+        bool is_sorted = true;
+        for(c = 0; c < n; c++) {
+            if (sorted[c] != copy_original_array[c]){
+                is_sorted = false;
+            }
 //            printf("%d ", sorted[c]);
-//
-//        }
-
-        printf("\n");
-        printf("\n");
-
-        /********** Clean up root **********/
+        }
+        printf(" Is array sorted?");
+        printf(is_sorted ? "true" : "false");
         free(sorted);
         free(other_array);
-
     }
-
-    /********** Clean up rest **********/
     free(original_array);
     free(sub_array);
     free(tmp_array);
 
-    /********** Finalize MPI **********/
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
 
 }
 
-/********** Merge Function **********/
 void merge(int *a, int *b, int l, int m, int r) {
 
     int h, i, j, k;
@@ -103,68 +98,40 @@ void merge(int *a, int *b, int l, int m, int r) {
     i = l;
     j = m + 1;
 
-    while((h <= m) && (j <= r)) {
-
-        if(a[h] <= a[j]) {
-
+    while ((h <= m) && (j <= r)) {
+        if (a[h] <= a[j]) {
             b[i] = a[h];
             h++;
-
-        }
-
-        else {
-
+        } else {
             b[i] = a[j];
             j++;
-
         }
-
         i++;
-
     }
 
-    if(m < h) {
-
-        for(k = j; k <= r; k++) {
-
+    if (m < h) {
+        for (k = j; k <= r; k++) {
             b[i] = a[k];
             i++;
-
         }
-
-    }
-
-    else {
-
-        for(k = h; k <= m; k++) {
-
+    } else {
+        for (k = h; k <= m; k++) {
             b[i] = a[k];
             i++;
-
         }
-
     }
-
-    for(k = l; k <= r; k++) {
-
+    for (k = l; k <= r; k++) {
         a[k] = b[k];
-
     }
-
 }
 
-/********** Recursive Merge Function **********/
 void mergeSort(int *a, int *b, int l, int r) {
-
+//    printf("some: ");
     int m;
-
     if (l < r) {
-
         m = (l + r) / 2;
-
         mergeSort(a, b, l, m);
         mergeSort(a, b, (m + 1), r);
         merge(a, b, l, m, r);
-
     }
 }
