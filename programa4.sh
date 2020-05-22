87f40834db768f0fa3ee7764bc015aa27816530b
simonass <simonas.stipinas@programmatus.com>
2020-05-22 11:57:24 +0300

    minor fixes

--- programa4.sh
+++ programa4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -p short # eilė
 #SBATCH -N1 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
-#SBATCH -c4 # kiek procesorių viename kompiuteryje
+#SBATCH -c12 # kiek procesorių viename kompiuteryje
 #SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
 mpicc -g -Wall -o main main.c
-mpiexec -n 8 ./main
+mpiexec -n 32 ./main 100000000
--- programa5.sh
+++ programa5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -p short # eilė
 #SBATCH -N1 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
-#SBATCH -c8 # kiek procesorių viename kompiuteryje
+#SBATCH -c12 # kiek procesorių viename kompiuteryje
 #SBATCH -C alpha # telkinys (jei alpha neveikia, bandykite beta)
 mpicc -g -Wall -o main main.c
-mpiexec -n 16 ./main
+mpiexec -n 32 ./main 70000000
